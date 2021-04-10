15b3e9626afded0444620ef101d4fb5579f9c8af
hp0404 <hryhorii.pavlenko@gmail.com>
2021-04-10 20:52:53 +0300

    min working example of datashader

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@
 import numpy as np
 import pandas as pd
 import streamlit as st
+import datashader as ds
 import matplotlib.colors
 import matplotlib.pyplot as plt
 from matplotlib.patches import Patch
@@ -15,13 +16,28 @@ def _to_hex(arr):
     return [matplotlib.colors.to_hex(c) for c in arr]
 
 
+def _get_extent(points):
+    min_x = np.min(points[:, 0])
+    max_x = np.max(points[:, 0])
+    min_y = np.min(points[:, 1])
+    max_y = np.max(points[:, 1])
+
+    extent = (
+        np.round(min_x - 0.05 * (max_x - min_x)),
+        np.round(max_x + 0.05 * (max_x - min_x)),
+        np.round(min_y - 0.05 * (max_y - min_y)),
+        np.round(max_y + 0.05 * (max_y - min_y)),
+    )
+    return extent
+
+
 def _select_dist(data, num):
     df = data.rename(columns={f"doc_top_reduced_{num}": "reduced_topics"}).copy()
     df = df.loc[:, ~df.columns.str.contains("doc_top_reduced_")]
     df["reduced_topics"] = pd.Categorical(df["reduced_topics"])
     return df
 
-def points(
+def matplotlib_points(
     data,
     num_topics,
     width=800, 
@@ -53,7 +69,47 @@ def points(
     ax.scatter(df["x"], df["y"], s=point_size, c=colors)
     ax.set_title(f"Reduced 2D embeddings: {num_topics} labeled topics")
     ax.legend(handles=legend_elements)
-    ax.margins(x=0, y=-.2)
+    # ax.margins(x=0, y=-.2)
+    ax.set(xticks=[], yticks=[])
+
+
+def datashader_points(
+    data,
+    num_topics,
+    width=800, 
+    height=800,
+):
+    dpi = plt.rcParams["figure.dpi"] 
+    fig = plt.figure(figsize=(800 / dpi, 800 / dpi))
+    ax = fig.add_subplot(111) 
+    
+    df = _select_dist(data, num_topics)
+    extent = _get_extent(df[["x", "y"]].values)
+    
+    cvs = ds.Canvas(
+        plot_width=width,
+        plot_height=height,
+        x_range=(extent[0], extent[1]),
+        y_range=(extent[2], extent[3]),
+    )
+    agg = cvs.points(df, "x", "y", agg=ds.count_cat("reduced_topics"))
+
+    unique_labels = df["reduced_topics"].unique()
+    num_labels = unique_labels.shape[0]
+    color_key = _to_hex(
+        plt.get_cmap("Spectral")(np.linspace(0, 1, num_labels))
+    )
+    legend_elements = [
+        Patch(facecolor=color_key[i], label=k)
+        for i, k in enumerate(sorted(unique_labels))
+    ]
+    result = ds.tf.shade(agg, color_key=color_key, how="eq_hist")
+    
+    #
+    ax.imshow(result.data.view(np.uint8).reshape(result.shape + (4,)))
+    ax.set_title(f"Reduced 2D embeddings: {num_topics} labeled topics")
+    ax.legend(handles=legend_elements)
+    # ax.margins(x=0, y=-.2)
     ax.set(xticks=[], yticks=[])
     
 
@@ -76,7 +132,9 @@ so that we could see clusters of documents""")
 st.sidebar.markdown("## Controls")
 st.sidebar.markdown("You can **change** the values to change the *chart*.")
 num_topic = st.sidebar.slider("Number of topics", min_value=2, max_value=20, step=1)
+method = st.sidebar.selectbox("Select plotting method", ("Matplotlib", "Datashader"))
 
 df = load_model()
-plot = points(df, num_topic)
+viz = matplotlib_points if method == "Matplotlib" else datashader_points
+plot = viz(df, num_topic)
 st.pyplot(plot)
